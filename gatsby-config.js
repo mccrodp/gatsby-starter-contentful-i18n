78dc37b07d6468b02f2153087b261bf626fc8f7c
Paul McCrodden <info@paulmccrodden.com>
2021-06-04 11:19:03 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: `rocybtov1ozk`,
-        accessToken: `6f35edf0db39085e9b9c19bd92943e4519c77e72c852d961968665f1324bfc94`,
+        spaceId: ``,
+        accessToken: ``,
       },
     },
     {
